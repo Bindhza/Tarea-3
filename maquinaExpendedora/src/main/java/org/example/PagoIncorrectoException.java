c90c2e7689daa697ab869edb7713de56ab845c6f
soda-caustica <maxbeltran@protonmail.com>
2025-05-02 21:25:06 -0400

    Cambio estructura de carpetas, cambio en enum, demostracion de metodo sort en main

--- maquinaExpendedora/src/main/java/org/example/PagoIncorrectoException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-/**
- * Excepcion lanzada cuando se paga con una moneda invalida
- */
-public class PagoIncorrectoException extends Exception {
-}
--- maquinaExpendedora/src/main/java/org/example/PagoInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-/**
- * Excepcion lanzada cuando el monto pagado no es suficiente
- */
-public class PagoInsuficienteException extends Exception {
-}
