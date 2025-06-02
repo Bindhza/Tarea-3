4bcba0772cf2ce18823e39e3926daca00d3282b4
Bindhza <nexxhurclan@gmail.com>
2025-06-02 14:21:30 -0400

    Base del boton

--- src/main/java/vista/PanelPrincipal.java
+++ src/main/java/vista/PanelPrincipal.java
@@ -12,11 +12,13 @@ public class PanelPrincipal extends JPanel {//se ve en el centro de la ventana
 
         panelExpendedor = new PanelExpendedor();
         panelComprador = new PanelComprador();
+        PanelBoton panelBoton = new PanelBoton();
 
         panelExpendedor.setPreferredSize(new Dimension(214,360));
         panelComprador.setPreferredSize(new Dimension(426,360));
         this.add(panelComprador);
         this.add(panelExpendedor);
+        this.add(panelBoton);
     }
 
 }
